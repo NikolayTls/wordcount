6a7b2219a86e38942cffa0aab5a2bfe3510cf4cd
Nikolay <bojkov98@gmail.com>
2021-07-12 21:15:10 +0300

    Initial commit

---
+++ wordcount/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request , 'home.html')
+
+def count(request):
+    fulltext = request.GET['fulltext']
+    wordlist=fulltext.split()
+
+    worddictionary={}
+
+    for word in wordlist:
+        if word in worddictionary:
+            #Increase
+            worddictionary[word]+=1
+        else:
+            #Add to dictionary
+            worddictionary[word]=1
+
+    worddictionary.items()
+
+    return render(request , 'count.html',{'fulltext':fulltext,'count':len(wordlist),'worddictionary':worddictionary.items()})
+
+def about(request):
+    return render(request, 'about.html')
